ff91e234355633f9088a25a7675b95380387ed24
production-jeu <43345835+production-jeu@users.noreply.github.com>
2019-02-08 17:09:40 -0500

    Animation AnimLettre marche

--- module_js/animLettre.js
+++ module_js/animLettre.js
@@ -8,15 +8,26 @@ export class AnimLettre
         this.animerLettres();
     }
     animerLettres(){
-        console.log(this.elmHeader);
+        let elmMot = document.createElement("div");
+        elmMot.classList.add("mot");
+        this.elmHeader.appendChild(elmMot);
         let i = 0;
-        for (let uneLettre of lesLettres){
-            //this.elmHeader
-            //uneLettre.classList.add("animNoel");
-			//uneLettre.style.animationDelay = (i * 0.5) + "s";
+        for (let uneLettre of this.lesLettres){
+            i++;
+            let nouvelleLettre = document.createElement("span");
+            nouvelleLettre.innerHTML = uneLettre;
+            nouvelleLettre.style.animationDelay = (i * 0.15) + "s";
+            nouvelleLettre.classList.add("classAnimLettre");
+            elmMot.appendChild(nouvelleLettre);
 		}
         /*for(let lettre of this.lesLettres){
             console.log(lettre);
         }*/
     }	
 }
+
+/**
+    let nouvelleLettre = document.createElement("span");
+    nouvelleLettre.innerHTML = uneLettre;
+    this.elmMot.appendChild(nouvelleLettre);
+ */
